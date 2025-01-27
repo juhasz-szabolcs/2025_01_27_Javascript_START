0229a4c0762a5d1fe84f40ccd835b6f81d71b4df
Szabolcs Juhász <juhasz.szabolcs@szbi-pg.hu>
2025-01-27 21:25:08 +0100

    a

---
+++ test.js
@@ -0,0 +1,18 @@
+console.log('test.js');
+console.log('Hello');
+
+// function katt() {
+//     document.write('<h1>PRÓ verzió!</h1>')
+// }
+
+function katt2() {
+    document.body.innerHTML = '<h1>PRÓ2 verzió!</h1>'
+}
+
+function kep_beilleszt() {
+    document.getElementById('kep_helye').innerHTML = '<img src=https://www.w3schools.com/images/w3schools_green.jpg>'
+}
+
+function kep_torol() {
+    document.getElementById('kep_helye').innerHTML = ''
+}
