5d13bed3ffcafe4261cf611dc23758ab9e8416fd
Szabolcs Juhász <juhasz.szabolcs@szbi-pg.hu>
2025-01-27 23:06:08 +0100

    változók létrehozása

---
+++ valtozok.js
@@ -0,0 +1,27 @@
+// A JavaScript nem típusos nyelv: a változóknak nem kell (és nem is tudjuk) megadni a típusát.
+// A változók megadása:
+
+//1. változó deklarálása (deklarálni kell használat előtt)
+var a = 2;
+console.log(a);
+
+//2. változó értékének módosítása
+let b = 3;
+b = 4;
+console.log('A változó értéke: ' + b);
+const c = 5;
+// c = 6; //hiba
+
+
+var x = 5;
+console.log(x);
+var x = "alma";
+console.log(x); //A var változók újradefiniálhatók.
+
+
+console.log(typeof(2)); // number
+console.log(typeof(true)); // boolean
+console.log(typeof('Aladár')); // string
+console.log(typeof({name:'Béla', age: 42})); // object
+console.log(typeof(function f(a, b) {return a + b})); // function
+console.log(typeof(nonexisting)); // undefined
